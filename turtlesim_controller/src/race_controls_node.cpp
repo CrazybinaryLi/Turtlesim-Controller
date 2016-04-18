531d70231deb22bf88c042c5134ddf46c399a90c
MTirtowidjojo <MTirtowidjojo@gmail.com>
2016-04-18 13:23:07 -0500

    Add way to change velocity

--- turtlesim_controller/src/race_controls_node.cpp
+++ turtlesim_controller/src/race_controls_node.cpp
@@ -1,7 +1,8 @@
 /**
  * Node to control turtlesim using a PS3 controller
- * Uses right and left triggers to move forwards and backwards
- * and uses the left thumbstick to turn the turtle
+ * Right and left triggers move the turtle forwards and backwards
+ * Left thumbstick turns the turtle
+ * D-Pad changes the linear and angular velocity
  */
 
 #include "ros/ros.h"
@@ -9,14 +10,39 @@
 #include "turtlesim/Pose.h"
 #include "sensor_msgs/Joy.h"
 
-#define BASE_LIN_VEL 2.0
-#define BASE_ANG_VEL 2.0
 #define HOR_AXES 0
-#define R_TRIGGER 9
+#define DPAD_UP 4
+#define DPAD_RIGHT 5
+#define DPAD_DOWN 6
+#define DPAD_LEFT 7
 #define L_TRIGGER 8
+#define R_TRIGGER 9
 
 bool received_joy = false;
+float lin_vel = 2.0;
+float ang_vel = 2.0;
 sensor_msgs::Joy input;
+
+/**
+ * Changes the linear and angular velocity of the turtle
+ * Up on the D-Pad increases linear velocity, down decreases linear velocity
+ * Right on the D-Pad increases angular velocity, left decreases angular velocity
+ */
+void update_vel() {
+    if (input.buttons[DPAD_UP] == 1) {
+        lin_vel *= 2;    
+        ROS_INFO("Lin vel: %.4f, Ang vel: %.4f", lin_vel, ang_vel);
+    } else if (input.buttons[DPAD_DOWN] == 1) {
+        lin_vel /= 2;
+        ROS_INFO("Lin vel: %.4f, Ang vel: %.4f", lin_vel, ang_vel);
+    } else if (input.buttons[DPAD_RIGHT] == 1) {
+        ang_vel *= 2;
+        ROS_INFO("Lin vel: %.4f, Ang vel: %.4f", lin_vel, ang_vel);
+    } else if (input.buttons[DPAD_LEFT] == 1) {
+        ang_vel /= 2;
+        ROS_INFO("Lin vel: %.4f, Ang vel: %.4f", lin_vel, ang_vel);
+    }
+}
 
 /**
  * Callback for the input from the controller
@@ -24,6 +50,7 @@ sensor_msgs::Joy input;
 void controller_cb(const sensor_msgs::Joy::ConstPtr& msg) {
     received_joy = true;
     input = *msg;
+    update_vel();
 }
 
 /**
@@ -35,14 +62,14 @@ void controller_cb(const sensor_msgs::Joy::ConstPtr& msg) {
  */
 void set_vel(geometry_msgs::Twist& turtle_vel) {
     if (input.buttons[R_TRIGGER] == 1) {
-        turtle_vel.linear.x = BASE_LIN_VEL;
+        turtle_vel.linear.x = lin_vel;
     } else if (input.buttons[L_TRIGGER] == 1) {
-        turtle_vel.linear.x = BASE_LIN_VEL * -1;
+        turtle_vel.linear.x = lin_vel * -1;
     } else {
         turtle_vel.linear.x = 0.0;
     }
     
-    turtle_vel.angular.z = BASE_ANG_VEL * input.axes[HOR_AXES];
+    turtle_vel.angular.z = ang_vel * input.axes[HOR_AXES];
 }
 
 int main(int argc, char** argv) {
@@ -50,7 +77,7 @@ int main(int argc, char** argv) {
     ros::NodeHandle n;
     ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1);
     ros::Subscriber controller_sub = n.subscribe("/joy", 1, controller_cb);
-    ros::Rate loop_rate(60);
+    ros::Rate loop_rate(10);
     geometry_msgs::Twist turtle_vel;
 
     // Waiting for controller_cb
