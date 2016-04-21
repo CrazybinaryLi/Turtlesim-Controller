94d7bc6ee8408363eec85215f0c4e8e0548ef457
MTirtowidjojo <MTirtowidjojo@gmail.com>
2016-04-20 19:15:26 -0500

    Update race controls

--- turtlesim_controller/src/race_controls_node.cpp
+++ turtlesim_controller/src/race_controls_node.cpp
@@ -15,18 +15,9 @@
 #define L_TRIGGER 8
 #define R_TRIGGER 9
 
-bool received_joy = false;
+geometry_msgs::Twist turtle_vel;
 Velocity vel;
-sensor_msgs::Joy input;
-
-/**
- * Callback for the input from the controller
- */
-void controller_cb(const sensor_msgs::Joy::ConstPtr& MSG) {
-    received_joy = true;
-    input = *MSG;
-    update_vel(vel, MSG->buttons);
-}
+bool received_joy = false;
 
 /**
  * Sets the velocity for the turtle
@@ -35,16 +26,25 @@ void controller_cb(const sensor_msgs::Joy::ConstPtr& MSG) {
  * Angular z is positive when left thumbstick is left, negative when 
  * left thumbstick is right, 0 when in center
  */
-void set_vel(geometry_msgs::Twist& turtle_vel) {
-    if (input.buttons[R_TRIGGER] == 1) {
+void set_vel(const int& R_TRIG, const int& L_TRIG, const float& ANG_MOD) {
+    if (R_TRIG == 1) {
         turtle_vel.linear.x = vel.linear;
-    } else if (input.buttons[L_TRIGGER] == 1) {
+    } else if (L_TRIG == 1) {
         turtle_vel.linear.x = vel.linear * -1;
     } else {
         turtle_vel.linear.x = 0.0;
     }
     
-    turtle_vel.angular.z = vel.angular * input.axes[HOR_AXES];
+    turtle_vel.angular.z = vel.angular * ANG_MOD;
+}
+
+/**
+ * Callback for the input from the controller
+ */
+void controller_cb(const sensor_msgs::Joy::ConstPtr& MSG) {
+    received_joy = true;
+    update_vel(vel, MSG->buttons);
+    set_vel(MSG->buttons[R_TRIGGER], MSG->buttons[L_TRIGGER], MSG->axes[HOR_AXES]);
 }
 
 int main(int argc, char** argv) {
@@ -53,7 +53,6 @@ int main(int argc, char** argv) {
     ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1);
     ros::Subscriber controller_sub = n.subscribe("/joy", 1, controller_cb);
     ros::Rate loop_rate(10);
-    geometry_msgs::Twist turtle_vel;
     vel.linear = 2.0;
     vel.angular = 2.0;
 
@@ -62,8 +61,8 @@ int main(int argc, char** argv) {
         ros::spinOnce();
     }
 
+    // Moving turtle
     while (ros::ok()) {
-        set_vel(turtle_vel);
         vel_pub.publish(turtle_vel);
         loop_rate.sleep();
         ros::spinOnce();
